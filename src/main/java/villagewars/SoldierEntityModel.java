031447e0ca382b08caa116473dc4750c0cdae31d
Arkon <kiekandrzej2@gmail.com>
2026-09-25 21:49:11 +0200

    Light_Infantry(oprócz tekstur)

--- src/main/java/villagewars/SoldierEntityModel.java
+++ src/main/java/villagewars/SoldierEntityModel.java
@@ -8,24 +8,40 @@ public class SoldierEntityModel extends BipedEntityModel<SoldierRenderState> {
 
     public SoldierEntityModel(ModelPart root) {
         super(root);
-        this.nose = root.getChild("head").getChild("nose");
-        this.head.xScale = 1.4F;
-        this.head.yScale = 1.4F;
-        this.head.zScale = 1.4F;
+
+        this.nose = root.getChild("nose");
 
     }
+
+    @Override
+    public void setAngles(SoldierRenderState state){
+        super.setAngles(state);
+
+        this.head.yScale = 1.4F;
+        this.head.xScale = 1.4F;
+        this.head.zScale = 1.4F;
+
+        this.nose.setOrigin(
+                this.head.originX,
+                this.head.originY,
+                this.head.originZ
+
+
+        );
+        this.nose.setAngles(this.head.pitch,this.head.yaw,this.head.roll);
+    }
+
+
 
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = BipedEntityModel.getModelData(new Dilation(0.0F),0.0F);
 
-        ModelPartData headData = modelData.getRoot().getChild("head");
 
-        headData.addChild(
-                "nose",
-                ModelPartBuilder.create().uv(0,0).cuboid(-1.0F,-2.0F,-6.0F, 2.0F, 2.0F, 2.0F,new Dilation(0.0F)),
-                ModelTransform.NONE
 
+        modelData.getRoot().addChild(
+                "nose",ModelPartBuilder.create().uv(56,16).cuboid(-1.0F, -3.0F, -7.6F, 2.0F, 4.0F, 2.0F),ModelTransform.NONE
         );
+
         return TexturedModelData.of(modelData,64,64);
     }
 }
